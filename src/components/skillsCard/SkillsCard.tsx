54df11f4bd93504c11faef34f65877fbc16557ff
Luca <lucagcuello@gmail.com>
2023-01-23 19:06:42 -0300

    Responsive changes Finished the 'Skills' responsive

--- src/components/skillsCard/SkillsCard.tsx
+++ src/components/skillsCard/SkillsCard.tsx
@@ -6,18 +6,15 @@ export const SkillsCard = ({
   name,
   mainColor,
   ligthColor,
-  textColor,
 }: {
   icon: any;
   name: string;
-  textColor?: string;
   mainColor: string;
   ligthColor: string;
 }) => {
   let colorStyles = {
     backgroundColor: `${mainColor}`,
     boxShadow: `40px 0px ${ligthColor}`,
-    color: `${textColor ? textColor : null}`,
   };
   return (
     <motion.div
