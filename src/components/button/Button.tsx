ad3617c62d5b7ec7d0f348d13f089157f967c8af
Luca <lucagcuello@gmail.com>
2023-02-23 22:22:29 -0300

    Added a dialog component to the resume button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,3 +1,10 @@
+import Button2 from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
 import "./Button.css";
 
 type ButtonProps = {
@@ -8,12 +15,57 @@ type ButtonProps = {
 };
 
 export const Button = ({ content, direction, icon, download }: ButtonProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handdleDownload = () => {
+    window.open(direction, "_blank");
+  };
+
   return (
-    <button className="comp-button">
-      {icon}
-      <a href={direction} download={download}>
-        {content}
-      </a>
-    </button>
+    <>
+      <button
+        className="comp-button"
+        onClick={() => {
+          download ? handleClickOpen() : null;
+        }}
+      >
+        {icon}
+        {download ? content : <a href={direction}>{content}</a>}
+      </button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Get resume?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You're about to get redirected to a PDF file with my resume. Do you want
+            to procede?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button2 onClick={handleClose}>No, thanks</Button2>
+          <Button2
+            onClick={() => {
+              handleClose();
+              handdleDownload();
+            }}
+            autoFocus
+          >
+            Yes!
+          </Button2>
+        </DialogActions>
+      </Dialog>{" "}
+    </>
   );
 };
