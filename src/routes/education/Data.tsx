49100a90a73c85cd5dcdf312bc670b36ffce7700
Luca <lucagcuello@gmail.com>
2023-11-24 20:49:34 -0300

    Removed unused code

--- src/routes/education/Data.tsx
+++ src/routes/education/Data.tsx
@@ -1,8 +1,3 @@
-import EnglishCertificate from "../../assets/certificates/EnglishCertificate.png";
-import ITSupportCertificate from "../../assets/certificates/ITSupportEspecialistCertificate.png";
-import ONECertificate from "../../assets/certificates/ONECertificate.png";
-import UXUICertificate from "../../assets/certificates/UXUICertificate.png";
-
 export const educationData = [
   {
     title: "University degree in Programming",
@@ -11,9 +6,6 @@ export const educationData = [
       "In this degree, I have learned to develop and improve algorithms using C++ and work on the production and enhancement of software. Additionally, I have acquired skills to analyze and solve data processing issues, debug and transfer processed information.",
     place: "Universidad Tecnológica Nacional",
     date: "05/2023 - Present",
-    certificateLink: "#",
-    certificateAvailible: false,
-    certificateImage: "",
   },
   {
     title: "Full-Stack Developer",
@@ -22,10 +14,6 @@ export const educationData = [
       "In this diploma, I'm currently improving my React skills and learning a new framework: Angular. As for the back-end, I'll learn NodeJS.",
     place: "UTN E-Learning",
     date: "08/2023 - Present",
-    certificateLink:
-      "https://app.aluracursos.com/program/certificate/58d5d3bb-8953-449a-b523-87a0bdea60e0",
-    certificateAvailible: true,
-    certificateImage: ONECertificate,
   },
   {
     title: "Oracle Next Education",
@@ -34,10 +22,6 @@ export const educationData = [
       "Crash course that helped to understand HTML/CSS/JavaScript more and it introduced me to a new programming language, Java, where I've learnt about Object-Oriented programming. I've also learnt Data Analytics with Python and SQL with MySQL Server.",
     place: "Oracle",
     date: "08/2022 - 02/2023",
-    certificateLink:
-      "https://app.aluracursos.com/program/certificate/58d5d3bb-8953-449a-b523-87a0bdea60e0",
-    certificateAvailible: true,
-    certificateImage: ONECertificate,
   },
   {
     title: "IT Support",
@@ -46,9 +30,5 @@ export const educationData = [
       "This specialization teached me a lot of new cool stuff, from installing hardware components to creating a virtual machine. I've learnt how to use different OS's, creating and executing scripts, troubleshooting and much more.",
     place: "Coursera, Google",
     date: "09/2022 - 01/2023",
-    certificateLink:
-      "https://www.coursera.org/account/accomplishments/specialization/certificate/YAPVP7AAUSJF",
-    certificateAvailible: true,
-    certificateImage: ITSupportCertificate,
   },
 ];
