db07e7ce4cbb952e8dd8e31856419c77d2916c59
Luca <lucagcuello@gmail.com>
2023-11-24 23:13:27 -0300

    Removed aboutme old files, updated aboutme styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { AboutMeContainer } from "./components/aboutMeContainer/AboutMeContainer";
 import { Footer } from "./components/footer/Footer";
 import { NavBar } from "./components/navBar/NavBar";
-import { ProfilePicture } from "./components/profilePicture/ProfilePicture";
 import { AboutMe } from "./views/about/AboutMe";
 import { Education } from "./views/education/Education";
 import { NotFound } from "./views/notFound/NotFound";
@@ -16,15 +14,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="*" element={<NotFound />} />
-        <Route
-          path="/home"
-          element={
-            <AboutMeContainer
-              body={<AboutMe />}
-              profilePicture={<ProfilePicture />}
-            />
-          }
-        />
+        <Route path="/home" element={<AboutMe />} />
         <Route path="/education" element={<Education />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
--- src/views/about/AboutMe.tsx
+++ src/views/about/AboutMe.tsx
@@ -1,52 +1,52 @@
 import { motion } from "framer-motion";
-import { AiOutlineMail } from "react-icons/ai";
-import { HiOutlineDocumentText } from "react-icons/hi";
-import CvEn from "../../assets/curriculum/CVFrontEN.pdf";
-import { Button } from "../../components/button/Button";
 import { NewButton } from "../../components/newButton/NewButton";
 import "./AboutMe.css";
 
 export const AboutMe = () => {
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8, ease: "backInOut" }}
-      className="about-me"
+      initial={{ opacity: 0, y: -200 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+      className="aboutme-container"
     >
-      <h1 className="heading">Luca</h1>
-      <h2 className="subheading">Front-end developer</h2>
-      <div className="buttons-container">
-        <Button
-          content="Resume"
-          direction={CvEn}
-          icon={<HiOutlineDocumentText />}
-          document={true}
-        ></Button>
-        <NewButton variant="primary" content="Resume" />
-        <NewButton variant="secondary" content="Contact me" />
-        <Button
-          content="Contact me"
-          direction="mailto:lucagcuello@gmail.com"
-          icon={<AiOutlineMail />}
-          document={false}
-        ></Button>
-      </div>
-      <main className="aboutme-body">
-        <p>
-          Hello and welcome to my portfolio! I’m a front-end developer who loves
-          crafting visually delightful and easy-to-use interfaces.
-        </p>
-        <p>
-          Currently, I'm pursuing a university degree and mastering full-stack
-          with my diploma. Every project refines my skills and knowledge. I aim
-          to deliver both functional and aesthetically pleasing code.
-        </p>
-        <p>
-          Take a look around, explore my projects, and let's chat! I’d love to
-          hear about how we might work together.
-        </p>
-      </main>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8, ease: "backInOut" }}
+        className="about-me"
+      >
+        <h1 className="heading">Luca</h1>
+        <h2 className="subheading">Front-end developer</h2>
+        <div className="buttons-container">
+          <NewButton variant="primary" content="My resume" />
+          <NewButton variant="secondary" content="Contact me!" />
+        </div>
+        <main className="aboutme-body">
+          <p>
+            Welcome to my portfolio! I’m a front-end developer who loves
+            crafting visually delightful and easy-to-use interfaces.
+          </p>
+          <p>
+            Currently, I'm working as a Vue&React developer at a Software
+            company, building websites and web apps. I'm also a student at the
+            National Technological University (UTN) of Argentina, where I'm
+            pursuing an associate degree in Programming.
+          </p>
+          <p>
+            Take a look at my projects and skills, and feel free to contact me
+            if you have any questions or would like to work together!
+          </p>
+        </main>
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.4 }}
+        className="picture-container"
+      >
+        {/* Acá iría mi foto */}
+      </motion.div>
     </motion.div>
   );
 };
